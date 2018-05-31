0901f278a4b3f0bb8b5ce294d21f1d41d3213b90
liliang <649743135@qq.com>
2018-05-31 09:30:05 +0800

    完善Json序列化器,默认截取前 3000 byte

--- src/main/java/uw/log/es/ser/ObjectAsStringSerializer.java
+++ src/main/java/uw/log/es/ser/ObjectAsStringSerializer.java
@@ -3,17 +3,27 @@ package uw.log.es.ser;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uw.httpclient.http.ObjectMapper;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
- * Json对象序列化成字符串而非Json对象
+ * Json对象序列化成字符串而非Json对象,默认截取前 3000 byte
+ * 主要用于序列化日志对象内嵌套对象的序列化
+ *
  * @author liliang
  * @since 2018-05-18
  */
 public class ObjectAsStringSerializer<T> extends JsonSerializer<T> {
+
+    private static final Logger logger = LoggerFactory.getLogger(ObjectAsStringSerializer.class);
+
+    /**
+     * UTF-8编码保存最大的字节数
+     */
+    private static final long MAX_BYTE_COUNT = 3000;
 
     @Override
     public void serialize(T value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
@@ -21,8 +31,16 @@ public class ObjectAsStringSerializer<T> extends JsonSerializer<T> {
         try {
             ObjectMapper.DEFAULT_JSON_MAPPER.write(buffer.outputStream(), value);
         } catch (Exception e) {
-            throw new IOException(e);
+            logger.error(e.getMessage(), e);
         }
-        gen.writeString(new InputStreamReader(buffer.inputStream()), (int) buffer.size());
+        long length = buffer.size();
+        String result;
+        if (length > MAX_BYTE_COUNT) {
+            result = buffer.readByteString(MAX_BYTE_COUNT).utf8();
+            buffer.clear();
+        } else {
+            result = buffer.readByteString().utf8();
+        }
+        gen.writeString(result);
     }
 }
